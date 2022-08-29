8be0f72cc4920a3e4d2d927548d4b8400dc0bb72
Tahsin020 <84002220+Tahsin020@users.noreply.github.com>
2022-08-29 18:21:57 +0300

    splash update

--- lib/view/authentication/splash/view/splash_view.dart
+++ lib/view/authentication/splash/view/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_groceries_app/product/color/app_color.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
@@ -21,7 +22,67 @@ class _SplashViewState extends State<SplashView> {
             width: MediaQuery.of(context).size.width,
             alignment: Alignment.center,
           ),
-          Image.asset("assets/images/carrot.png")
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    children: [
+                      Image.asset("assets/images/carrot.png"),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.03,
+                      ),
+                      Text(
+                        "Welcome",
+                        style: Theme.of(context).textTheme.headline3?.copyWith(
+                              color: Colors.white,
+                            ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.01,
+                      ),
+                      Text(
+                        "to our store ",
+                        style: Theme.of(context).textTheme.headline3?.copyWith(color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.01,
+                      ),
+                      Text(
+                        "Ger your groceries in as fast as one hour",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            ?.copyWith(color: const Color(0xffb3fcfcfc), fontSize: 16, fontWeight: FontWeight.normal),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.02,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.08,
+                        width: MediaQuery.of(context).size.width * 0.85,
+                        child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              primary: AppColor.glen,
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(19)),
+                            ),
+                            child: Text(
+                              "Get Started",
+                              style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.white, fontSize: 18),
+                            )),
+                      ),
+                      const Padding(padding: EdgeInsets.all(40))
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          )
         ],
       ),
     );
